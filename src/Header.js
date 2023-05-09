e60278a6c6ef5baf39dbb643ce46ee3935cf64e3
Wan Nabil <wannabil.xyz@gmail.com>
2023-05-09 20:13:36 +0800

    update-header

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header id="main-header">
+      <div className="logo">
+        <a href="index.html"><img src="logo.png" alt="Logo"></a>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">Accesories</a></li>
+          <li><a href="#">Apparel</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Sell To Us</a></li>
+          <li><a href="#">Sneakers</a></li>
+          <li><a href="#">Whatsize Merch</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
